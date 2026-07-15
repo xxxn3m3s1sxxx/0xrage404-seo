e53bd161589b790ebcb4802485473756b935671c
Atlas Researcher <research@atlas.nomad>
2026-07-15 17:22:28 +0200

    fix: canvas z-index -1, remove body stacking context

--- src/matrix.ts
+++ src/matrix.ts
@@ -30,7 +30,7 @@ export class MatrixRain {
   }
 
   attach(): this {
-    this.canvas.style.cssText = "position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:0;pointer-events:none;";
+    this.canvas.style.cssText = "position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:-1;pointer-events:none;";
     document.body.prepend(this.canvas);
     return this;
   }
